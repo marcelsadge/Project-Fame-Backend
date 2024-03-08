bdb405822c41c1639c2aa81a6fe6cfbc8f26cc94
marcelsadge <zjunhao@seas.upenn.edu>
2024-03-08 10:57:26 -0500

    Update connection string

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const fameCutoffRoute = require('./routes/cutoff.routes');
 const moeRoute = require('./routes/moe.routes');
 
 const port = process.env.PORT || 3000;
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/WotCluster';
+const uri = 'mongodb://test:test@ac-snvzbge-shard-00-00.erpjyn6.mongodb.net:27017,ac-snvzbge-shard-00-01.erpjyn6.mongodb.net:27017,ac-snvzbge-shard-00-02.erpjyn6.mongodb.net:27017/?ssl=true&replicaSet=atlas-u5ei8l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=WoTFame';
 
 const app = express();
 app.use(express.json());
